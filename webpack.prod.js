1b033260cbeab4dc8070ca107761d11da7a9331f
hidmantra <derivdiggs@gmail.com>
2018-10-21 01:30:27 -0400

    removed plugin preventing production bundle from export

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
-const webpack = require('webpack/index.js');
 const merge = require('webpack-merge');
+//const webpack = require('webpack/index.js');
 const common = require('./webpack.common.js');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
@@ -10,11 +10,11 @@ module.exports = merge(common, {
   devtool: 'source-map',
   plugins: 
   [
-    
+    /*
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-    
+    */
     new FaviconsWebpackPlugin({
       logo: './src/THR-logo-bw.png',
       persistentCache: false,
